9c5b688b84bc1227db88867365e28b296c3fd1d0
YingCGooi <gooiyingchyi@gmail.com>
2018-08-19 12:05:18 -0400

    remove Table component, create columns as props to Table

--- src/App.js
+++ src/App.js
@@ -1,45 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import Data from './data.js';
+import Table from './components/Table.js'
 
 class App extends Component {
-  getAirline(route) {
-    const airline = Data.getAirlineById(route.airline);
-    return airline.name;
-  }
-
-  getAirport(code) {
-    const airport = Data.getAirportByCode(code);
-    return airport.name;
-  }
-
   render() {
+    const columns = [
+      {name: 'Airline', property: 'airline'},
+      {name: 'Source Airport', property: 'src'},
+      {name: 'Destination Airport', property: 'dest'},
+    ];
+
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <table>
-            <thead>
-              <tr>
-                <th>Airline</th>
-                <th>Source</th>
-                <th>Destination</th>
-              </tr>
-            </thead>          
-            <tbody>
-            {
-              Data.routes.map((route, i) => (
-                <tr key={i}>
-                  <td>{this.getAirline(route)}</td>
-                  <td>{this.getAirport(route.src)}</td>
-                  <td>{this.getAirport(route.dest)}</td>
-                </tr>
-              ))
-            }
-            </tbody>
-          </table>
+          <Table 
+            className="routes-table"
+            columns={columns}
+            rows=""
+            format=""
+          />
         </section>
       </div>
     );
