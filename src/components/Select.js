0cbed9b31132aa7d10f0c92cd84c3faf021f4eeb
YingCGooi <gooiyingchyi@gmail.com>
2018-08-20 15:37:17 -0400

    change value, key and title to reference props.valueKey

---
+++ src/components/Select.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Select = (props) => (
+  <div className='field'>
+    <label htmlFor={props.name}>
+      {props.label}
+    </label>
+    <select name={props.name} onChange={props.onChange}>
+      <option value='all'>{props.allTitle}</option>
+      {
+        props.options.map(option => (
+          <option 
+            disabled={option.disabled} 
+            value={option[props.valueKey]} 
+            key={option[props.valueKey]}
+          >
+            {option[props.titleKey]}
+          </option>
+        ))
+      }
+    </select>
+  </div>
+)
+
+export default Select;
