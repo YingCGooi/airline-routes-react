bc162f996a40ea1c54fde1e602a5d8d2523fdffa
YingCGooi <gooiyingchyi@gmail.com>
2018-08-20 15:35:19 -0400

    add ability to filter routes based on states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard'
+import Form from './components/Form'
 import Data from './data'
 
 class App extends Component {
   state = {
-    airline = null;
+    airlineId: 'all',
+    airportCode: 'all'
+  }
+
+  filterRoutes = () => {
+    return Data.routes.filter(route => {
+      const airlineId = this.state.airlineId;
+      const airportCode = this.state.airportCode;
+      let airlineCriteria = true;
+      let airportCriteria = true;
+
+      if (airlineId !== 'all') airlineCriteria = route.airline === +(this.state.airlineId);
+      if (airportCode !== 'all') airportCriteria = [route.src, route.dest].includes(this.state.airportCode);
+
+      return airlineCriteria && airportCriteria;
+    });
   }
 
   formatValue(property, value) {
@@ -16,6 +32,12 @@ class App extends Component {
     }
   }
 
+  handleSelect = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(_ => ({ [name]: value }));
+  }
+
   render() {
     const columns = [
       {name: 'Airline', property: 'airline'},
@@ -23,19 +45,28 @@ class App extends Component {
       {name: 'Destination Airport', property: 'dest'},
     ];
 
+    const filteredRoutes = this.filterRoutes();
+    const filteredAirlines = Data.airlines;
+    const filteredAirports = Data.airports;
+
     return (
       <div>
         <header className='ui inverted square menu'>
           <h3 className='header item'>Airline Routes</h3>
         </header>
         <main className='ui container'>
-          <Select 
-
+          <Form 
+            airlineOptions={filteredAirlines} 
+            airportOptions={filteredAirports}
+            value=""
+            onSelect={this.handleSelect} 
           />
+
+          <div className='ui divider' />
 
           <Dashboard
             columns={columns}
-            rows={''}
+            rows={filteredRoutes}
             format={this.formatValue}
             perPage={25}
           />
